522be734ba5719fef550d95b1a4a2fbef08eac11
morhaham <you@example.com>
2025-07-07 01:53:22 +0300

    wip

--- .config/nvim/lua/plugins/ui/arrow.lua
+++ .config/nvim/lua/plugins/ui/arrow.lua
@@ -9,6 +9,7 @@ return {
     show_icons = true,
     leader_key = ";", -- Recommended to be a single key
     buffer_leader_key = "m", -- Per Buffer Mappings
+    save_key = "git_root",
     window = {
       border = "rounded",
     },
